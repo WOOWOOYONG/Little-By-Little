167bb261a3a057346f7de422fb2f487d2ab1e993
WOOWOOYONG <woowooyong@gmail.com>
2022-10-28 20:27:25 +0800

    10/28[done] DOM練習-擲骰子

---
+++ JavaScript/JS P14 Roll The Dice/Readme.md
@@ -0,0 +1,3 @@
+DOM 練習-擲骰子數字總和
+
+[Demo](https://rolling-dicessss.surge.sh/)
---
+++ JavaScript/JS P14 Roll The Dice/script.js
@@ -0,0 +1,30 @@
+const images = [
+  "/dice-svg/dice-01.svg",
+  "/dice-svg/dice-02.svg",
+  "/dice-svg/dice-03.svg",
+  "/dice-svg/dice-04.svg",
+  "/dice-svg/dice-05.svg",
+  "/dice-svg/dice-06.svg",
+];
+
+const dice = document.querySelectorAll("img");
+
+function roll() {
+  dice.forEach((die) => {
+    die.classList.add("shake");
+  });
+  setTimeout(() => {
+    dice.forEach((die) => {
+      die.classList.remove("shake");
+    });
+    let dieOneValue = Math.floor(Math.random() * 6);
+    let dieTwoValue = Math.floor(Math.random() * 6);
+    console.log(dieOneValue, dieTwoValue);
+    document.querySelector("#throw").setAttribute("src", "");
+    document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+    document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+    document.querySelector("#total").innerHTML = `Your roll is ${
+      dieOneValue + 1 + (dieTwoValue + 1)
+    }`;
+  }, 1000);
+}
