4d831ead913449a23bfc26c37e082096b7bab7ee
woowooyong <woowooyong@gmail.com>
2023-02-19 14:10:54 +0800

    update: Vue基本語法練習

---
+++ Vue-Basics1/README.md
@@ -0,0 +1,34 @@
+# modal-project
+
+## Vue 基本語法練習
+
+- v-if
+- v-on
+- v-bind
+- slot
+- components
+- emit
+- props
+- teleport
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ Vue-Basics1/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:modal1Content>
+        <h3>This is Modal1</h3>
+      </template>
+      <h2>Hello Modal1</h2>
+    </Modal>
+  </teleport>
+  <teleport to=".modals" v-if="showModal2">
+    <Modal @close="toggleModal2">
+      <h3>This is Modal2</h3>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModal2">showmodal2</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App',
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
+</script>
+
+<style>
+#app,
+.modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
