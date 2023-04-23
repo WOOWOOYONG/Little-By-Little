a6cc51fcc963c1c40a6cc6ddb6f0ef45a562e03f
woowooyong <woowooyong@gmail.com>
2023-04-23 11:01:48 +0800

    使用ESmodule，把需要的匯率資料放到exchange.js

---
+++ Vue-ExchangeRate-Practice/exchange.js
@@ -0,0 +1,23 @@
+const exchangeInfo = {
+  USD: ['美金'],
+  HKD: ['港幣'],
+  GBP: ['英鎊'],
+  AUD: ['澳幣'],
+  CAD: ['加拿大幣'],
+  SGD: ['新加坡幣'],
+  CHF: ['瑞士法郎'],
+  JPY: ['日圓'],
+  ZAR: ['南非幣'],
+  SEK: ['瑞典克朗'],
+  NZD: ['紐元'],
+  THB: ['泰銖'],
+  PHP: ['菲律賓披索'],
+  IDR: ['印尼盾'],
+  EUR: ['歐元'],
+  KRW: ['韓元'],
+  VND: ['越南盾'],
+  MYR: ['馬來幣'],
+  CNY: ['人民幣'],
+};
+
+export default exchangeInfo;
